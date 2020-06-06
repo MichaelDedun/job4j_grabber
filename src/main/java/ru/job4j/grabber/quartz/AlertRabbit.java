73ff5e2b38ff38690ad63c3593098a06a50b216c
Michael Dedun <michael.dedun@mail.ru>
2020-06-06 19:59:22 +0300

    Задание [260360#270149]

--- src/main/java/ru/job4j/grabber/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/grabber/quartz/AlertRabbit.java
@@ -4,6 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.FileInputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -17,10 +20,19 @@ public class AlertRabbit {
         try (FileInputStream in = new FileInputStream("./src/main/resources/rabbit.properties")) {
             config.load(in);
         }
-        try {
+        Connection connection = DriverManager.getConnection(
+                config.getProperty("jdbc.url"),
+                config.getProperty("jdbc.username"),
+                config.getProperty("jdbc.password"));
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("store", store);
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
                     .repeatForever();
@@ -29,15 +41,31 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException | NumberFormatException e) {
-            e.printStackTrace();
-        }
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(store);
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
-        public void execute(JobExecutionContext context) {
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDataMap dataMap = context.getJobDetail().getJobDataMap();
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            store.add(System.currentTimeMillis());
+            Connection connection = (Connection) dataMap.get("connection");
+            try (PreparedStatement ps = connection.prepareStatement("INSERT INTO rabbit (created_date) VALUES (?)", Statement.RETURN_GENERATED_KEYS)) {
+                ps.setLong(1, store.get(0));
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
