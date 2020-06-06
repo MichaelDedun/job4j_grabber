cc8f67eb23b7e599e7949aa7b220cc3b8f1042e2
Michael Dedun <michael.dedun@mail.ru>
2020-06-06 18:18:49 +0300

    Задание [175122#270145]

---
+++ src/main/java/ru/job4j/grabber/quartz/AlertRabbit.java
@@ -0,0 +1,44 @@
+package ru.job4j.grabber.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    public static void main(String[] args) throws Exception {
+        Properties config = new Properties();
+        try (FileInputStream in = new FileInputStream("./src/main/resources/rabbit.properties")) {
+            config.load(in);
+        }
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+}
