cbea6bf664b6f344f6605b18e1ab0ca935e69bf1
Michael Dedun <michael.dedun@mail.ru>
2020-06-07 17:50:18 +0300

    Задание [260358#270147]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,24 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements rowsWithDate = doc.select(".altCol");
+        for (int i = 0; i < row.size(); i++) {
+            Element td = row.get(i);
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element data = rowsWithDate.get(i);
+            System.out.println(data.childNodes().get(0));
+        }
+    }
+
+}
