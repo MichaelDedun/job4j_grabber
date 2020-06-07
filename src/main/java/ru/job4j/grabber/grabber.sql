6eafa56c8be13e268fa28dd4afc23bf3aacda4a2
Michael Dedun <michael.dedun@mail.ru>
2020-06-07 18:06:39 +0300

    Задание [1731#270150]

--- src/main/java/ru/job4j/grabber/grabber.sql
+++ src/main/java/ru/job4j/grabber/grabber.sql
@@ -2,3 +2,11 @@ create table rabbit (
 	id SERIAL PRIMARY KEY,
 	created_date BIGINT
 );
+
+create table post (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    text VARCHAR(255),
+    link VARCHAR (255) UNIQUE ,
+    created_date TIMESTAMP 
+);
